ade506b335500b03fa6c82322173febc94f4f2af
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-05-28 20:44:39 -0300

    requisito 05 parcialmente concluido, falta testes

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,18 +3,54 @@ import ProductModel, {
 import { Product } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
 
+function validateName({ name }: ProductInputtableTypes): ServiceResponse<Product> | null {
+  // validando o campo name:
+  if (!name) return { status: 'INVALID_DATA', data: { message: '"name" is required' } };
+  
+  if (typeof name !== 'string') { 
+    return { status: 'UNPROCESSABLE_ENTITY', data: { message: '"name" must be a string' } }; 
+  }
+
+  if (name.length < 3) {
+    return { 
+      status: 'UNPROCESSABLE_ENTITY', 
+      data: { message: '"name" length must be at least 3 characters long' }, 
+    };
+  }
+
+  return null;
+}
+
+function validatePrice({ price }: ProductInputtableTypes): ServiceResponse<Product> | null {
+  if (!price) return { status: 'INVALID_DATA', data: { message: '"price" is required' } };
+  
+  if (typeof price !== 'string') { 
+    return { status: 'UNPROCESSABLE_ENTITY', data: { message: '"price" must be a string' } }; 
+  }
+
+  if (price.length < 3) {
+    return { 
+      status: 'UNPROCESSABLE_ENTITY', 
+      data: { message: '"price" length must be at least 3 characters long' }, 
+    };
+  }
+  
+  return null;
+}
+
 async function registerProduct(product: ProductInputtableTypes): Promise<ServiceResponse<Product>> {
-  if (!product.name) return { status: 'INVALID_DATA', data: { message: '"name" is required' } };
-  if (!product.price) return { status: 'INVALID_DATA', data: { message: '"price" is required' } };
-
+  const nameError = validateName(product);
+  if (nameError) return nameError;
+  
+  const priceError = validatePrice(product);
+  if (priceError) return priceError;
+  
   const newProduct = await ProductModel.create(product);
-
   return { status: 'SUCCESSFUL', data: newProduct.dataValues };
 }
 
 async function getProducts(): Promise<ProductSequelizeModel[]> {
   const products = await ProductModel.findAll();
-
   return products;
 }
 
